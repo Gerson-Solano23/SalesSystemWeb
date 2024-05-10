b53a645189756ae36087e531bcf7b9c799cc62ac
Gerson-Solano <83689574+Gerson-Solano@users.noreply.github.com>
2024-05-09 18:19:12 -0600

    feat: add dependency injection
    creation dependecy class in IOC Layer, add StringConnection to the services

---
+++ backend/SalesSystem/SalesSystem.IOC/Dependencia.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SalesSystem.DAL.DbSalesContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesSystem.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependecias(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DBSalesContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("StringConnection"));
+            });
+        }
+    }
+}
