17fa516ae341ff7d5fd3319da7638e7f1e2edde5
Gerson-Solano23 <159967377+Gerson-Solano23@users.noreply.github.com>
2024-05-30 16:11:13 -0600

    feat: add contrat and service rolfeat: add contrat and service category
    creation of service and contrat for the logic process in the system

---
+++ backend/SalesSystem/SalesSystem.BLL/Services/CategoryService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SalesSystem.BLL.Services.Contract;
+using SalesSystem.DAL.Repositories.Contract;
+using SalesSystem.DTO;
+using SalesSystem.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesSystem.BLL.Services
+{
+    public class CategoryService : ICategory
+    {
+
+        private readonly IGenericRepository<Category> _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IGenericRepository<Category> repositoryCategory, IMapper mapper)
+        {
+            _mapper = mapper;
+            _categoryRepository = repositoryCategory;
+        }
+        public async Task<List<CategoryDTO>> List()
+        {
+            try
+            {
+                var queryUsuario = await _categoryRepository.Consult();
+                var categoryList = queryUsuario.Include(rol => rol.IdCategory).ToList();
+
+                return _mapper.Map<List<CategoryDTO>>(categoryList);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ backend/SalesSystem/SalesSystem.BLL/Services/Contract/ICategory.cs
@@ -0,0 +1,18 @@
+﻿using SalesSystem.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesSystem.BLL.Services.Contract
+{
+    public interface ICategory
+    {
+        Task<List<CategoryDTO>> List();
+        //Task<CategoryDTO> Create(CategoryDTO entity);
+        //Task<bool> Update(CategoryDTO entity);
+        //Task<bool> Delete(int id);
+        //Task<CategoryDTO> Get(int id);
+    }
+}
