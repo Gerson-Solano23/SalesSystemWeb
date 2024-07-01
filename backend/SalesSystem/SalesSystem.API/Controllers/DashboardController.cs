2c0e0e284ad181fc264e0b768d194160aae9b4e7
Gerson-Solano23 <159967377+Gerson-Solano23@users.noreply.github.com>
2024-07-01 10:00:49 -0600

    feat: add dashboard to api
    service and contract

---
+++ backend/SalesSystem/SalesSystem.API/Controllers/DashboardController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SalesSystem.API.Utility;
+using SalesSystem.BLL.Services.Contract;
+using SalesSystem.DTO;
+
+namespace SalesSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashBoard _dashBoardService;
+
+        public DashboardController(IDashBoard dashBoardService)
+        {
+            _dashBoardService = dashBoardService;
+        }
+
+        [HttpGet]
+        [Route("Summary")]
+        public async Task<IActionResult> Summary()
+        {
+            var response = new Response<DashboardDTO>();
+
+            try
+            {
+                response.status = true;
+                response.data = await _dashBoardService.Summary();
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.message = ex.Message;
+                return BadRequest(response);
+            }
+        }
+    }
+}
