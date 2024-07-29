65348d84e9f79f5b1c3aea43fde65c6cf4d7b09b
Gerson-Solano23 <159967377+Gerson-Solano23@users.noreply.github.com>
2024-07-29 16:46:19 -0600

    feat: add new dtos to create new feature
    new creation module to dashboard

---
+++ backend/SalesSystem/SalesSystem.DTO/MonthlySalesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesSystem.DTO
+{
+    public class MonthlySalesDTO
+    {
+        public string? Month { get; set; }
+        public int? Total { get; set; }
+    }
+}
