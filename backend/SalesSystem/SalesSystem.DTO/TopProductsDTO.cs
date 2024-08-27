120530557c6ed993a1f67e616abf0f46c4e2e73b
Gerson-Solano23 <159967377+Gerson-Solano23@users.noreply.github.com>
2024-08-27 16:37:07 -0600

    feat: add entity to use data
    this entity management the top of products with a dinamic logic

---
+++ backend/SalesSystem/SalesSystem.DTO/TopProductsDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesSystem.DTO
+{
+    public class TopProductsDTO
+    {
+        [Key]
+        public int topNumber { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public int salequantity { get; set; }     
+        public decimal totalSale { get; set; }
+    }
+}
