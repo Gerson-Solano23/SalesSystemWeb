fd50d625afd9afdf98491ba5e20ab3502027a969
Gerson-Solano23 <159967377+Gerson-Solano23@users.noreply.github.com>
2024-08-27 16:20:10 -0600

    feat: add ranges entities
    this entities show data per date range

---
+++ backend/SalesSystem/SalesSystem.DTO/DashboardRangeDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesSystem.DTO
+{
+    public class DashboardRangeDTO
+    {
+        public int TotalSales { get; set; }
+        public string TotalRevenue { get; set; }
+        public int TotalProducts { get; set; }
+        public List<RangeDateDTO> RangeSales { get; set; }
+
+        public List<TopProductsDTO> topProducts { get; set; }
+    }
+}
