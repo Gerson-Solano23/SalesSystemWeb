34b973abf2f3946b3fe5220f24e4530a7506e2ca
Gerson-Solano23 <159967377+Gerson-Solano23@users.noreply.github.com>
2024-08-27 16:24:43 -0600

    feat: add new rearly entities
    this entities show data per year

---
+++ backend/SalesSystem/SalesSystem.DTO/DashboardYearlyDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesSystem.DTO
+{
+    public class DashboardYearlyDTO
+    {
+        public int TotalSales { get; set; }
+        public string TotalRevenue { get; set; }
+        public int TotalProducts { get; set; }
+        public List<YearlySalesDTO> YearlySales { get; set; }
+        public List<TopProductsDTO> topProducts { get; set; }
+    }
+}
