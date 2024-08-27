17d56a83c81eb141882693880f299d118b53f495
Gerson-Solano23 <159967377+Gerson-Solano23@users.noreply.github.com>
2024-08-27 15:53:13 -0600

    feat: add entity to use in management files
    new entity model, using in upload file to cloud storage

---
+++ backend/SalesSystem/SalesSystem.Entity/S3DetailsFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesSystem.Entity
+{
+    public class S3DetailsFile
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime? FileDate { get; set; }
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+    }
+}
